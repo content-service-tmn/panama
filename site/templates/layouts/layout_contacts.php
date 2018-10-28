e094044386568c8d9e7c34f5cba8232501b67f53
vasilstar <vasilstar97@gmail.com>
2018-10-28 23:43:53 +0500

    контакты

--- site/templates/layouts/layout_contacts.php
+++ site/templates/layouts/layout_contacts.php
@@ -1,29 +1,38 @@
+<?php namespace Processwire; ?>
+
 <section class="contacts">
   <div class="contacts__info">
     <div class="container">
-      <h1 class="contacts__title">контакты</h1>
+      <h1 class="contacts__title"><?= $page->main_title ?></h1>
       <div class="contacts__grid">
         <div class="contacts__cell">
-          <p class="contacts__subtitle">как добраться?</p>
+          <p class="contacts__subtitle"><?= $page->contacts_transport_title ?></p>
           <ul class="contacts__list">
-            <li><b>Автобусы:</b> 10, 15, 22, 8</li>
-            <li><b>Маршрутное такси:</b>79, 96, 120, 79, 96, 79, 96, 24</li>
+            <?php $tmp = $page->contacts_transport; ?>
+            <?php foreach($tmp as $item):?>
+              <li><b><?= $item->contacts_transport_name ?>: </b><?= $item->contacts_transport_numbers ?></li>
+            <?php endforeach; ?>
           </ul>
         </div>
         <div class="contacts__cell">
-          <p class="contacts__subtitle">время работы</p>
+          <p class="contacts__subtitle"><?= $page->contacts_schedule_title ?></p>
           <ul class="contacts__list">
-            <li><b>Ежедневно</b> с <span>10:00</span> до <span>22:00</span></li>
-            <li><b>Гипермаркет</b> с <span>09:00</span> до <span>21:00</span></li>
-            <li><b>Кинотеатр</b> с <span>09:00</span> до <span>02:00</span></li>
+            <?php $tmp = $page->contacts_schedule; ?>
+            <?php foreach($tmp as $item): $time = explode(',', $item->contacts_schedule_time)?>
+              <li><b><?= $item->contacts_schedule_name ?></b> c <span><?= $time[0] ?></span> до <span><?= $time[1] ?></span></li>
+            <?php endforeach; ?>
           </ul>
         </div>
         <div class="contacts__cell">
-          <p class="contacts__subtitle">контакты</p>
+          <p class="contacts__subtitle"><?= $page->contacts_contacts_title ?></p>
           <ul class="contacts__list">
-            <li><b><a href="">trcpanama@gmail.com</a></b></li>
-            <li><b><a href="">+7 (3452) 520-779</a></b></li>
-            <li>г. Тюмень, ул. 2-я Луговая, д. 30</li>
+            <?php foreach(explode(',', $page->contacts_email) as $mail):?>
+              <li><a href="mailto:<?=$mail?>"><?=$mail?></a></li>
+            <?php endforeach; ?>
+            <?php foreach(explode(',', $page->contacts_phone) as $phone):?>
+              <li><a href="tel:<?=phoneLink($phone)?>"><?=$phone?></a></li>
+            <?php endforeach; ?>
+            <li><?= $page->contacts_address; ?></li>
           </ul>
         </div>
       </div>
@@ -34,7 +43,7 @@
 
 <script type="text/javascript">
     function initMap() {
-        var coordinates = {lat: 57.1715732, lng: 65.5399764},
+        var coordinates = {lat: <?= $page->latitude; ?>, lng: <?= $page->longitude; ?>},
             options = {
                 zoom: 16,
                 disableDefaultUI: true,
