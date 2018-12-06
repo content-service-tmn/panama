a4dba744f6c00e361bb9200631883ae7bbd546b3
vasilstar <vasilstar97@gmail.com>
2018-12-07 01:39:53 +0500

    123

--- site/templates/layouts/layout_scheme.php
+++ site/templates/layouts/layout_scheme.php
@@ -57,10 +57,10 @@
           </div>
           <div class="map__group">
             <img class="map__floor" src="<?= $config->urls->templates . 'assets/img/first-floor.png'?>" alt="">
-            <div class="map__marker map__marker_exit" style="left:10%; top:20%"></div>
-            <div class="map__marker map__marker_exit" style="left:30%; top:20%"></div>
-            <div class="map__marker map__marker_service" style="left:25%; top:40%"></div>
-            <div class="map__marker map__marker_exit" style="left:35%; top:10%"></div>
+            <div class="map__marker map__marker_exit" style="left:10%; top:20%" data-id="1"></div>
+            <div class="map__marker map__marker_exit" style="left:30%; top:20%" data-id="2"></div>
+            <div class="map__marker map__marker_service" style="left:25%; top:40%" data-id="3"></div>
+            <div class="map__marker map__marker_exit" style="left:35%; top:10%" data-id="4"></div>
           </div>
         </div>
       </div>
@@ -71,10 +71,40 @@
         <div class="map__catalog">
           <ul>
             <li>Магазины</li>
+            <li data-id="2">Перекрёсток</li>
+            <li data-id="3">Галамарт</li>
+            <li data-id="4">Ostin</li>
+          </ul>
+          <ul>
+            <li>Магазины</li>
             <li>Перекрёсток</li>
             <li>Галамарт</li>
             <li>Ostin</li>
           </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="info" data-id="1">
+  <div class="container">
+    <div class="info__grid">
+      <div class="info__cell">
+        <div class="info__img" style="background-image:url(<?= $config->urls->templates . 'assets/img/pic.jpg'?>)"></div>
+        <ul class="info__list">
+          <li style="background-image:url(<?= $config->urls->templates . 'assets/img/clock_white.svg'?>)">с 10:00 до 22:00</li>
+          <li style="background-image:url(<?= $config->urls->templates . 'assets/img/phone_white.svg'?>)"><a href="">+7 (999) 000-00-00</a></li>
+          <li style="background-image:url(<?= $config->urls->templates . 'assets/img/email_white.svg'?>)"><a href="">ecco.store@gmail...</a></li>
+          <li style="background-image:url(<?= $config->urls->templates . 'assets/img/browser_white.svg'?>)"><a href="">www.ecco.com</a></li>
+        </ul>
+        <p class="info__category">женская одежда</p>
+      </div>
+      <div class="info__cell">
+        <p class="info__subtitle">МАГазин</p>
+        <h1 class="info__title">ECco</h1>
+        <div class="info__content">
+          <p>причём уровень контроля над Силой различен в зависимости от врождённых способностей и тренированности. Сила описана в фильме Оби-Ваном Кеноби как «энергетическое поле, создаваемое всеми живыми существами, которое окружает нас, находится внутри нас, и связывает воедино Галактику». Человек, способный ощущать Силу, при условии тренировок с юного возраста, может научиться направлять Силу и, пройдя в Ордене испытания, получить звание рыцаря-джедая. Владение Силой — это управление окружающим пространством (энергия, материя, мысли), влияние на это пространство.</p>
         </div>
       </div>
     </div>
--- site/templates/src/js/map.js
+++ site/templates/src/js/map.js
@@ -3,8 +3,40 @@ $(document).ready(function(){
   var groups = $('.map__group');
   var active = -1;
   ChangeActive(0);
+
+  $('.map__search input').on('input',function(){
+    $('.map__catalog li').removeClass('hidden');
+    $('.map__catalog ul').removeClass('hidden');
+    var substr = $(this).val().toUpperCase();
+    $('.map__catalog li').each(function(){
+      if ($(this).is(':first-child')) return;
+      var str = $(this).text().toUpperCase();
+      if (str.indexOf(substr)!=0) $(this).addClass('hidden')
+    });
+    $('.map__catalog ul').each(function(){
+      if ($(this).find('li').length-1==$(this).find('li.hidden').length) $(this).addClass('hidden');
+    });
+  });
+  $('.map__marker').click(function(){
+    var id = $(this).attr('data-id');
+    $(".info").removeClass('active');
+    $(".info[data-id='"+id+"']").addClass('active');
+    $('html, body').animate({
+       scrollTop: $(".info[data-id='"+id+"']").offset().top
+   }, 500);
+  });
+  $('.map__catalog li:not(:first-child)').click(function(){
+    var id = $(this).attr('data-id');
+    console.log(id);
+    var group = $('.map__marker[data-id="'+id+'"]').closest('.map__group');
+    var index = $('.map__group').index(group);
+    ChangeActive(2-index);
+    $('.map__marker').removeClass('fade');
+    group.find('.map__marker[data-id!="'+id+'"]').addClass('fade');
+
+  });
   function ChangeActive(index) {
-    if (index<0 || index>2) return;
+    if (index<0 || index>2 || index==active) return;
     active = index;
     tabs.removeClass('active');
     tabs.eq(active).addClass('active');
@@ -27,7 +59,7 @@ $(document).ready(function(){
         groups.eq(1).addClass('hide');
         break;
     }
-    $('.map__marker').removeClass('visible');
+    $('.map__marker').removeClass('visible').removeClass('fade');
     var markers = groups.eq(2-index).find(".map__marker");
     var delay = 500.0/markers.length;
     var i = 0;
